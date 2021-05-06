8868e27d3a93a789093e0102f8d1ee687e5fe8e9
learner1998 <40159457+learner1998@users.noreply.github.com>
2021-05-06 19:55:37 +0530

    removed a lil bit of code

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -8,14 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
   constructor(private movieService: MovieService) {}
-  default = false;
   movies: any;
-  defaultImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png';
   ngOnInit(): void {
     this.movieService.data.subscribe((res: any) => {
-      if (res.poster_path == undefined) {
-        res.poster_path = this.defaultImage;
-      }
       this.movies = res;
     });
   }
